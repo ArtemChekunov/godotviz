2d1f071f650fa01e714be45aaba27dd9b00b29a6
Artem Chekunov <scorp.dev.null@gmail.com>
2015-08-02 19:58:27 +0300

    first commit

---
+++ godotviz.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	result := DotRender("graph {a -- b a -- b  b -- a }", "png")
+	fmt.Print(result.String())
+}
+
+func DotRender(inputString string, outputType string) bytes.Buffer {
+	bufferIn := bytes.NewBufferString(inputString)
+	var bufferOut bytes.Buffer
+
+	cmd := exec.Command("dot", "-T"+outputType)
+
+	cmd.Stdin = bufferIn
+	cmd.Stdout = bufio.NewWriter(&bufferOut)
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
+	return bufferOut
+
+}
